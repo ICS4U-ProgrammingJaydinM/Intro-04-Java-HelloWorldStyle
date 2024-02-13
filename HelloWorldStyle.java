f493093e8367290724ca76bd18a16574e8c850a6
Jaydin Madore <JaydinMadore@stu.ocsb.ca>
2024-02-13 15:57:42 +0000

    “HelloWorldStyle”

---
+++ HelloWorldStyle.java
@@ -0,0 +1,14 @@
+/**
+ * Prints Hello world and goodbye to Kent
+ *
+ * @author Jaydin Madore
+ * @version 1.0
+ * @since 2024/02/13
+ */
+public class HelloWorldStyle {
+  public static void main(String[] args) {
+
+    System.out.print("Hello world\n");
+    System.out.print("goodbye Kent\n");
+  }
+}
