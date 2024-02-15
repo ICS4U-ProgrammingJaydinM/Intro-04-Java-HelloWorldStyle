6ca79012a8c933f56799e159ea094a89de66ac18
Jaydin Madore <JaydinMadore@stu.ocsb.ca>
2024-02-15 15:58:30 +0000

    HelloWorldStyle

---
+++ com/example/HelloWorldStyle.java
@@ -0,0 +1,29 @@
+package com.example;
+
+/**
+ * Prints Hello world and goodbye to Kent
+ *
+ * @author Jaydin Madore
+ * @version 1.0
+ * @since 2024/02/13
+ */
+
+
+// HelloWorldStyle class
+public final class HelloWorldStyle {
+
+  /** Private constructor to prevent instantiation. */
+  private HelloWorldStyle() {
+    throw new UnsupportedOperationException("Cannot instantiate");
+  }
+
+  /**
+   * This is the main method.
+   *
+   * @param args Unused
+   */
+  public static void main(final String[] args) {
+    System.out.println("Hello Kent\n");
+    System.out.println("goodbye Kent\n");
+  }
+}
